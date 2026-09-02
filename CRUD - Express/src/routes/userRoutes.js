5741d9498468b7e988a23c872fd0522868ab4122
phantom-gitc <sarojkumarpadhi01@gmail.com>
2026-09-02 20:36:04 +0530

    Advance CRUD

--- CRUD - Express/src/routes/userRoutes.js
+++ CRUD - Express/src/routes/userRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
-    createUser,
-    getUsers,
-    getUser,
-    updateUser,
-    deleteUser
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
 } from "../controllers/userController.js";
 import {
-    validateCreateUser,
-    validateUpdateUser,
-    validateId
+  validateCreateUser,
+  validateUpdateUser,
+  validateId,
 } from "../middleware/userValidation.js";
 
 const router = express.Router();
@@ -20,12 +20,5 @@ router.get("/get-users", getUsers);
 router.get("/get-user/:id", validateId, getUser);
 router.put("/update-user/:id", validateId, validateUpdateUser, updateUser);
 router.delete("/delete-user/:id", validateId, deleteUser);
-
-// REST routes
-router.post("/", validateCreateUser, createUser);
-router.get("/", getUsers);
-router.get("/:id", validateId, getUser);
-router.put("/:id", validateId, validateUpdateUser, updateUser);
-router.delete("/:id", validateId, deleteUser);
 
 export default router;
